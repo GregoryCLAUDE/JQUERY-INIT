cbd0e8545cf91b779607fff4d064bca9edff4c11
Grégory <claudejurado@orange.fr>
2017-03-28 14:37:12 +0200

    dernier exercice , last step, juste avant la correction, ouf! changement de couleur

--- js/app.js
+++ js/app.js
@@ -12,3 +12,8 @@ $("#setText").on("change",function(){
 $("div.needText").html($("#setText").val())
 	
 });
+
+
+$("#setColor").click(function(){
+	$("#changeColor").css("background-color","red");
+});
